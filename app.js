720bceeb421452b60275c9f2a97f7a3c41f79cae
kauannsm <kauannsm13@gmail.com>
2020-10-18 15:18:14 -0300

    redirecionar os acessos para HTTPS

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 app.get('/', (req, res) => { 
-  res.redirect('https://' + req.headers.host + req.url);
   res.send('Hello World!')
 });
 
