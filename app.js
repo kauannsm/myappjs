e2321d36e95c7e0cbb6c49b27004988540166359
kauannsm <kauannsm13@gmail.com>
2020-10-18 15:13:41 -0300

    redirecionar os acessos para HTTPS

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
+  res.redirect('https://' + req.headers.host + req.url);
   res.send('Hello World!')
 });
 
 app.listen(port, () => {
   
-  app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
-      res.redirect('https://domminio.tk' + req.url); //Redireciona pra HTTPS
-    else //Se a requisição já é HTTPS
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-});
-
   console.log('npm script test');
 });
